202bc87bc73d22abb539c1428d528d3b0251dab3
Adrien Matissart <a@matissart.net>
2022-04-26 12:05:17 +0200

    clean up and fix background color on hover

--- frontend/src/components/entity/style.ts
+++ frontend/src/components/entity/style.ts
@@ -4,7 +4,6 @@ export const entityCardMainSx: SxProps = {
   margin: 0,
   width: '100%',
   maxWidth: 1000,
-  background: '#FFFFFF',
   border: '1px solid #DCD8CB',
   boxShadow: '0px 0px 8px rgba(0, 0, 0, 0.02), 0px 2px 4px rgba(0, 0, 0, 0.05)',
   borderRadius: '4px',
